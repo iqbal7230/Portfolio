17de68812e674e38e32594afd3afd23f6e1d7a78
iqbal7230 <iqbalxply@gmail.com>
2025-01-25 22:59:02 +0530

    UI modification

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-10 pt-20">
+    <div className="pb-5 pt-20">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,12 +7,12 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20" id='projects'>
+    <div className="py-10" id='projects'>
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-12 mt-8">
         {projects.map((item) => (
           <Link
             href={item.link}
